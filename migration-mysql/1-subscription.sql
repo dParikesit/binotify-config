211ddfb198f259e2c575611cc3ce717472706b0a
dParikesit <dsparikesit@gmail.com>
2022-11-28 17:34:15 +0700

    feat: rest compose

--- migration-mysql/1-subscription.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS subs (
-    creator_id INT NOT NULL,
-    subscriber_id INT NOT NULL,
-    status ENUM("PENDING", "ACCEPTED", "REJECTED") DEFAULT "PENDING" NOT NULL,
-    CONSTRAINT PK_Subs PRIMARY KEY (creator_id, sub_id)
-);
